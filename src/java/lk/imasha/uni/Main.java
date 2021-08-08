cb885ab570332bc3075ebc24b52e82586dded220
Imasha Weerakoon <67216030+imashaWe@users.noreply.github.com>
2021-08-08 12:28:15 +0530

    implemented text reading functions

--- src/java/lk/imasha/uni/Main.java
+++ src/java/lk/imasha/uni/Main.java
@@ -1,7 +1,37 @@
 package lk.imasha.uni;
 
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-       System.out.println("Hello wold");
+     final TextReader textReader = new TextReader("D:\\Projects\\Uni Projects\\Software Constructions\\calculator\\numbers.text");
+     final Scanner scanner = new Scanner(System.in);
+     double num1 = Double.parseDouble(textReader.readLine()[0]);
+     double num2 = Double.parseDouble(textReader.readLine()[1]);
+     boolean isRunning = true;
+
+     while (isRunning) {
+         System.out.print("Enter the method that you want (add/sub/mul):");
+         final String method = scanner.nextLine();
+         switch (method) {
+             case "add":
+                 System.out.println( MathFunction.add(num1, num2));
+                 isRunning = false;
+                 break;
+             case "sub":
+                 System.out.println(MathFunction.sub(num1, num2));
+                 isRunning = false;
+                 break;
+             case "mul":
+                 System.out.println(MathFunction.mul(num1, num2));
+                 isRunning = false;
+                 break;
+             default:
+                 System.out.println("Invalid Entry");
+
+         }
+     }
+
     }
 }
